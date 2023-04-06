ae571fb360c5d5af3d651573a40f9aedc3e0f9c5
bhupender613 <bhupender.choudhary72@gmail.com>
2023-04-06 22:56:17 +0530

    add images

--- src/admin/entities/tag.entity.ts
+++ src/admin/entities/tag.entity.ts
@@ -1,45 +1,47 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { ApiProperty } from '@nestjs/swagger';
-import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { ApiProperty } from "@nestjs/swagger";
+import { Document } from "mongoose";
 
 export type TagDocument = Tag & Document;
 @Schema({
-    timestamps: true,
-    versionKey: false
+  timestamps: true,
+  versionKey: false,
 })
 export class Tag {
-    @Prop()
-    @ApiProperty()
-    title: string;
+  @Prop()
+  @ApiProperty()
+  title: string;
 
-    @Prop()
-    @ApiProperty()
-    metaTitle: string;
+  @Prop()
+  @ApiProperty()
+  metaTitle: string;
 
-    @Prop()
-    @ApiProperty()
-    metaDescription: string;
+  @Prop()
+  @ApiProperty()
+  metaDescription: string;
 
-    @Prop()
-    @ApiProperty()
-    description: string;
-    
-    @Prop({ default: false })
-    @ApiProperty()
-    addToMenu: boolean;
+  @Prop()
+  @ApiProperty()
+  description: string;
 
-    @Prop({ default: false })
-    @ApiProperty()
-    enable: boolean;
+  @Prop({ default: false })
+  @ApiProperty()
+  addToMenu: boolean;
 
-    @Prop({ default: false })
-    @ApiProperty()
-    isDeleted: boolean;
+  @Prop()
+  @ApiProperty()
+  tagImage: string;
 
-    @Prop({ default: false })
-    @ApiProperty()
-    isBlocked: boolean;
+  @Prop({ default: false })
+  @ApiProperty()
+  enable: boolean;
 
-    
+  @Prop({ default: false })
+  @ApiProperty()
+  isDeleted: boolean;
+
+  @Prop({ default: false })
+  @ApiProperty()
+  isBlocked: boolean;
 }
 export const TagSchema = SchemaFactory.createForClass(Tag);
